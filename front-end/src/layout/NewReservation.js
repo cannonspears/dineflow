f58c61525ca057f97bdb48f9c38fcf84d4eaa083
Cannon Spears <cannonspears@gmail.com>
2023-06-21 12:00:53 -0500

    Created form and implemented new reservation page

--- front-end/src/layout/NewReservation.js
+++ front-end/src/layout/NewReservation.js
@@ -1,9 +1,41 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import ReservationForm from "./ReservationForm";
 
 function NewReservation() {
+  const initialFormData = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: "",
+  };
+
+  const history = useHistory();
+  const [formData, setFormData] = useState({ ...initialFormData });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <main>
       <h1>New Reservation</h1>
+      <ReservationForm
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        formData={formData}
+        path={"/"}
+      />
     </main>
   );
 }
---
+++ front-end/src/layout/ReservationForm.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function ReservationForm({ handleChange, handleSubmit, formData, path }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="first_name">First Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="first_name"
+          name="first_name"
+          onChange={handleChange}
+          value={formData.first_name}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="last_name">Last Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="last_name"
+          name="last_name"
+          onChange={handleChange}
+          value={formData.last_name}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="mobile_number">Mobile Number</label>
+        <input
+          type="text"
+          className="form-control"
+          id="mobile_number"
+          name="mobile_number"
+          onChange={handleChange}
+          value={formData.mobile_number}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="reservation_date">Reservation Date</label>
+        <input
+          type="date"
+          className="form-control"
+          id="reservation_date"
+          name="reservation_date"
+          onChange={handleChange}
+          value={formData.reservation_date}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="reservation_time">Reservation Time</label>
+        <input
+          type="time"
+          className="form-control"
+          id="reservation_time"
+          name="reservation_time"
+          onChange={handleChange}
+          value={formData.reservation_time}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="people">Number in Party</label>
+        <input
+          type="number"
+          className="form-control"
+          id="people"
+          name="people"
+          onChange={handleChange}
+          value={formData.people}
+        />
+      </div>
+
+      <Link to={path} type="button" className="btn btn-secondary mr-2" role="button">
+        Cancel
+      </Link>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default ReservationForm;
