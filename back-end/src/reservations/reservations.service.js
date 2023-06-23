9aec2de40611a83d566a14336ba54e56d653f20d
Cannon Spears <cannonspears@gmail.com>
2023-06-23 14:50:19 -0500

    Finished POST request functionality

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -4,6 +4,14 @@ function list() {
   return knex("reservations").select("*");
 }
 
+function create(reservation) {
+  return knex("reservations")
+    .insert(reservation)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
 module.exports = {
   list,
+  create,
 };
