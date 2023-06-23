02ebcd9a0b13e4e4af34588859ef9ff81ac9c9fe
Cannon Spears <cannonspears@gmail.com>
2023-06-23 14:41:22 -0500

    Created POST request to reservations/new

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,9 +1,48 @@
 const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+
+const hasRequiredProperties = hasProperties(
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people"
+);
+
+const VALID_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+];
+
+function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  const invalidFields = Object.keys(data).filter((field) => !VALID_PROPERTIES.includes(field));
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  next();
+}
 
 async function list(req, res) {
   res.json({ data: await service.list() });
 }
 
+async function create(req, res) {
+  res.status(201).json({ data: await service.create(req.body.data) });
+}
+
 module.exports = {
   list,
+  create: [hasOnlyValidProperties, hasRequiredProperties, asyncErrorBoundary(create)],
 };
