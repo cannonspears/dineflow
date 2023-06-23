12235e6027d3f44d5301fb04e3a6d1167844d4d9
Cannon Spears <cannonspears@gmail.com>
2023-06-23 14:27:21 -0500

    Created Error handlers

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
