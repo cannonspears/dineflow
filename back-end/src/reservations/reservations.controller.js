1ea728c9ea3ccd889d87da9c7352e9e5db942f15
Cannon Spears <cannonspears@gmail.com>
2023-07-10 10:28:29 -0500

    fix create request

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -38,19 +38,7 @@ async function list(req, res) {
   res.json({ data: await service.list() });
 }
 
-function nextId() {
-  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
-  return uint32.toString(16);
-}
-
 async function create(req, res) {
-  // const newReservation = req.body.data;
-
-  // const now = new Date().toISOString();
-  // newReservation.reservation_id = nextId();
-  // newReservation.created_at = now;
-  // newReservation.updated_at = now;
-
   res.status(201).json({
     data: await service.create(req.body.data),
   });
