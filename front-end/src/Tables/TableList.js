80d24d442ed07b2435f3b38286281ce5c9633641
Cannon Spears <cannonspears@gmail.com>
2023-07-17 13:14:20 -0500

    Table List table

--- front-end/src/Tables/TableList.js
+++ front-end/src/Tables/TableList.js
@@ -7,7 +7,28 @@ function TableList({}) {
       <td>{table.capacity}</td>
     </tr>
   ));
-  return <h1>Table List</h1>;
+  return (
+    <main>
+      <div className="d-md-flex mt-3">
+        <h4>Tables</h4>
+      </div>
+      <div className="table-responsive">
+        <table className="table table-sm w-75 text-center mb-5">
+          <thead>
+            <tr>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Mobile Number</th>
+              <th scope="col">Reservation Date</th>
+              <th scope="col">Reservation Time</th>
+              <th scope="col">People</th>
+            </tr>
+          </thead>
+          <tbody className="table-group-divider">{reservationsRows}</tbody>
+        </table>
+      </div>
+    </main>
+  );
 }
 
 export default TableList;
