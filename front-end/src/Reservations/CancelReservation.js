d5684ca39556d94292c1638b49dff6ea478948c5
Cannon Spears <cannonspears@gmail.com>
2023-07-20 02:25:42 -0500

    Reformatting Cancel component

--- front-end/src/Reservations/CancelReservation.js
+++ front-end/src/Reservations/CancelReservation.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+// Import Utility Functions
 import { cancelReservation } from "../utils/api";
 
 function CancelReservation({ reservation_id, loadDashboard }) {
   const abortController = new AbortController();
+
   const handleCancel = async () => {
     const confirm = window.confirm(
       "Do you want to cancel this reservation?\nThis cannot be undone."
