f428f5772c034428650b0d4763e88b69e189d56c
Cannon Spears <cannonspears@gmail.com>
2023-07-21 10:29:14 -0500

    Fixed routing errors

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -28,20 +28,20 @@ function Routes() {
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
-      <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
-      </Route>
       <Route path="/dashboard">
         <Dashboard date={date ? date : today()} />
       </Route>
       <Route path="/reservations/new">
         <NewReservation />
       </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <SeatReservation />
+      </Route>
       <Route path="/reservations/:reservation_id/edit">
         <EditReservation />
       </Route>
-      <Route path="/reservations/:reservation_id/seat">
-        <SeatReservation />
+      <Route exact={true} path="/reservations">
+        <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/tables/new">
         <NewTable />
